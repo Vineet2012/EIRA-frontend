f88c9c7e7686bd2818368a213e69fffc3fe576f9
Vineet2012 <panwarvineet2012@gmail.com>
2024-01-19 11:03:09 +0530

    data changed

--- src/pages/Shop/ShopPageActivityCmp.js
+++ src/pages/Shop/ShopPageActivityCmp.js
@@ -11,7 +11,7 @@ export default function ShopPageActivityCmp() {
       <ShopPageCardCmp
         title="Sunset speed boating tour and beach hopping"
         content="Relish a unique blend of relaxation and luxury on this exotic vacation at the Standard"
-        image="https://www.aethos.com/ericeira/wp-content/uploads/AethosEriceira3956_2000x1300-1024x684.jpg"
+        image="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/54/06/31/caption.jpg?w=1200&h=-1&s=1"
         timeFromHotel={"2km from hotel"}
         hours={"3 hour"}
         time={"5:00PM"}
@@ -20,7 +20,7 @@ export default function ShopPageActivityCmp() {
       <ShopPageCardCmp
         title="Spa and sauna access"
         content="Huruvalhi, Maldives."
-        image="https://www.aethos.com/ericeira/wp-content/uploads/Aethos-Ericeira_pool_DSC7180-Edytuj_web-1024x683.jpg"
+        image="https://ik.imagekit.io/bi5ihkjoby38/prod-activities/activities/846df43b-c2a5-4dad-b49d-7c13c12d78f6/22bf2279-113a-4bd2-b69c-b73a18cc4540/161103_Westin_Nashville_0214s8tc.jpg?tr=w-1920"
         timeFromHotel={"0km from hotel"}
         hours={"24 hour access"}
         time={"n/a"}
